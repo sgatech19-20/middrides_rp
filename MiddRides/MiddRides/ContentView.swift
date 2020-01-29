9c0de12518b29918bca9cb1848aa3ab30ac5743c
Lizeth Lucero <lizethlucero@yahoo.com>
2020-01-29 00:14:27 -0500

    Created xcode project

---
+++ MiddRides/MiddRides/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiddRides
+//
+//  Created by Lizeth Lucero on 1/29/20.
+//  Copyright © 2020 SGA Technology. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
